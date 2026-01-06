f3f9012e8e9477c874799f7e53bf7145fdbc83e6
brfialho <brfialho@student.42sp.org.br>
2026-01-05 21:07:28 -0300

    includes to header

--- main.h
+++ main.h
@@ -6,13 +6,17 @@
 /*   By: brfialho <brfialho@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/01/05 20:07:45 by brfialho          #+#    #+#             */
-/*   Updated: 2026/01/05 20:22:15 by brfialho         ###   ########.fr       */
+/*   Updated: 2026/01/05 21:07:08 by brfialho         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef MAIN_H
 # define MAIN_H
 
-#include "libft.h"
+# include "libft.h"
+# include <fcntl.h>
+# include <sys/stat.h>
+# include <sys/wait.h>
+
 
 #endif
