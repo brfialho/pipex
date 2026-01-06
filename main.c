776e99b31efaf7de653be61e0bb407524d089b92
brfialho <brfialho@student.42sp.org.br>
2026-01-05 22:00:58 -0300

    finished parser

--- main.c
+++ main.c
@@ -6,13 +6,44 @@
 /*   By: brfialho <brfialho@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/01/05 20:05:36 by brfialho          #+#    #+#             */
-/*   Updated: 2026/01/05 20:27:34 by brfialho         ###   ########.fr       */
+/*   Updated: 2026/01/05 21:59:41 by brfialho         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "main.h"
 
-int	main(void)
+void	parse_cmd_lst(t_pipex *pipex, int argc, char **argv)
 {
-	ft_printf("Hello World\n");	
+	int	i;
+
+	pipex->cmd = ft_calloc(1, sizeof(t_list **));
+	if (!pipex->cmd)
+		exit(2);
+	i = 1;
+	while (++i < argc - 1)
+		lst_add_end(pipex->cmd, lst_new_node(ft_strdup(argv[i])));
+}
+
+
+void	parsing(t_pipex *pipex, int argc, char **argv)
+{
+	if (argc < 5)
+		exit(1);
+	parse_cmd_lst(pipex, argc, argv);
+	pipex->input.path = argv[1];
+	pipex->output.path = argv[argc - 1];
+}
+void	print_node(void *content)
+{
+	ft_printf("%s\n", (char *)content);
+}
+
+int	main(int argc, char **argv)
+{
+	t_pipex	pipex;
+
+	parsing(&pipex, argc, argv);
+	lst_for_each(*pipex.cmd, print_node);
+	lst_del_all(pipex.cmd, free);
+	free(pipex.cmd);
 }
--- main.h
+++ main.h
@@ -6,7 +6,7 @@
 /*   By: brfialho <brfialho@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/01/05 20:07:45 by brfialho          #+#    #+#             */
-/*   Updated: 2026/01/05 21:07:08 by brfialho         ###   ########.fr       */
+/*   Updated: 2026/01/05 21:18:51 by brfialho         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,5 +18,17 @@
 # include <sys/stat.h>
 # include <sys/wait.h>
 
+typedef struct s_file
+{
+	char	*path;
+	int		fd;
+}	t_file;
+
+typedef struct s_pipex
+{
+	t_file	input;
+	t_file	output;
+	t_list	**cmd;
+}	t_pipex;
 
 #endif
